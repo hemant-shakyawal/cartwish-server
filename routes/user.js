072e91f8d8633a4f571dd0897d9941a50709f3a8
Hemant kumar shakyawal <hemantkumarshakyawal@gmail.com>
2026-01-14 19:19:09 +0530

    add hashpassword and joi validate userschema

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
+
+const Joi = require('joi');
+
+const createUserSchema = Joi.object({
+    name: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    delveryAddress: Joi.string().min(5).required()
+});
 
 // Create a new user
 router.post('/', async (req, res) => {
     try {
         const { name, email, password, delveryAddress } = req.body;
+        const joyValidation = createUserSchema.validate(req.body);
+        if (joyValidation.error) {
+            return res.status(400).json({ message: joyValidation.error.details[0].message });
+        }
         const user = await User.findOne({ email: email });
 
 
         if (user) {
             return res.status(400).json({ message: 'User already exists' });
         }
+
+        const hashPassword = await bcrypt.hash(password, 10);
         const newUser = new User({
             name: name,
             email: email,
-            password: password,
+            password: hashPassword,
             delveryAddress: delveryAddress
         });
 
