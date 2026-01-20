dc428db5490dad0a974fffdd86eb6387949d3a6f
Hemant kumar shakyawal <hemantkumarshakyawal@gmail.com>
2026-01-20 23:23:50 +0530

    facebook login

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -9,6 +10,17 @@ passport.use(new GoogleStrategy({
     passReqToCallback: true
 },
     function (request, accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+    }
+));
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3200/api/auth/facebook/callback",
+    passReqToCallback: true,
+    profileFields: ['id', 'emails', 'name', 'displayName', 'picture.type(large) ']
+},
+    function (accessToken, refreshToken, profile, cb) {
         return done(null, profile);
     }
 ));
--- routes/auth.js
+++ routes/auth.js
@@ -39,4 +39,38 @@ router.get('/google/callback', passport.authenticate('google', {
     res.redirect(`http://localhost:4200/dashboard?token=${token}`);
 });
 
+router.get('/facebook',
+    passport.authenticate('facebook', { scope: ['public_profile', 'email'] }));
+
+router.get('/facebook/callback', passport.authenticate('facebook', {
+    session: false,
+    failureRedirect: 'http://localhost:4200/login',
+}), async (req, res) => {
+    //check usser is avilable  or not using google id or mail
+    const profile = req.user;
+
+    const user = await User.findOne({ facebookId: profile.id }, { email: profile.emails[0].value });
+
+    if (user) {
+        // User  is avilable -update  google id & genrate token and send it in response
+        if (!user.facebookId) {
+            user.facebookId = profile.id;
+            await user.save();
+        }
+
+    } else {
+        // user is not avilable - create new user and genrate token and send it in response
+        const newUser = new User({
+            facebookId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+        });
+        await newUser.save({ validateBeforeSave: false });
+    }
+
+    const token = jwt.sign({ _id: user._id, name: user.name }, process.env.JWT_SECRET, {
+        expiresIn: '2h',
+    });
+    res.redirect(`http://localhost:4200/dashboard?token=${token}`);
+});
 module.exports = router;
